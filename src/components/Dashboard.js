9e1730368cc278da1cd06c39e3baf923d46b584d
sourabhgupta385 <gupta.sourabh385@gmail.com>
2018-03-19 21:08:38 +0530

    Menu added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,15 +2,41 @@
  * Created by manish on 19/3/18.
  */
 import React, {Component} from 'react';
-
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                wewe
-            </div>
-        );
-    }
+    constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
+  render() {
+    return (
+        
+      <div>
+          <AppBar
+                title="Smart Share"
+                iconClassNameRight="muidocs-icon-navigation-expand-more"
+                onLeftIconButtonClick={this.handleToggle}/>
+        
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
