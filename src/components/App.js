300e85594b8dcf98a3568688a13bfb2353ebaad5
manishbisht <manish.bisht490@gmail.com>
2018-03-19 21:09:38 +0530

    firebase login added

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,86 @@
 import React, {Component} from 'react';
-import AppBar from 'material-ui/AppBar';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import * as firebase from "firebase";
+import * as FirebaseUI from 'firebaseui'
+var config = {
+    apiKey: "AIzaSyDkfRXm3IqwYtirAqear7IX9v0mJ8HkjNM",
+    authDomain: "skit-rajasthan-hackathon.firebaseapp.com",
+    databaseURL: "https://skit-rajasthan-hackathon.firebaseio.com",
+    projectId: "skit-rajasthan-hackathon",
+    storageBucket: "skit-rajasthan-hackathon.appspot.com",
+    messagingSenderId: "834983110145"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            username: '',
-            password: '',
             currentUser: null,
             isLoggedIn: false,
-            error: '',
-            currentBoard: null,
+            error: null,
         };
+
+        this.registerLogin = this.registerLogin.bind(this);
+    }
+
+    checkLogin() {
+        let self = this;
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                self.setState({
+                    user: user,
+                    isLoggedIn: true
+                });
+            }
+            else {
+                self.setState({
+                    isLoggedIn: false
+                });
+            }
+        });
+    }
+
+    registerLogin() {
+        let self = this;
+        let uiConfig = {
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                firebase.auth.GithubAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                firebase.auth.PhoneAuthProvider.PROVIDER_ID
+            ],
+            'callbacks': {
+                signInSuccess: function(currentUser, credential, redirectUrl) {
+                    self.setState({
+                        user: currentUser,
+                        isLoggedIn: true
+                    });
+                }
+            },
+        };
+        let ui = new FirebaseUI.auth.AuthUI(firebase.auth());
+        ui.start('#firebaseui-auth-container', uiConfig);
+    }
+
+    componentWillMount() {
+        this.checkLogin();
     }
 
     render() {
-        return (
-            <div>
-                <AppBar
-                title="Smart Share"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"/>
-            </div>
-        );
+        if (this.state.isLoggedIn) {
+            return (
+                <Dashboard />
+            );
+        } else {
+            return (
+                <Login registerLogin={this.registerLogin} />
+            );
+        }
     }
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,21 @@
  * Created by manish on 19/3/18.
  */
 import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+import '../css/firebaseui.css';
 
 class Login extends Component {
+    componentWillMount() {
+        this.props.registerLogin();
+    }
+
     render() {
         return (
             <div>
-                asas
+                <AppBar
+                    title="Smart Share"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+                <div id="firebaseui-auth-container"></div>
             </div>
         );
     }
